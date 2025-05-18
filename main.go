aacfdee1b6b9b28018957853dd6a3c36c418da2f
Alya221 <azinovey@mail.ru>
2025-05-18 20:33:20 +0300

    add: solve 3 tasks

--- main.go
+++ main.go
@@ -1,59 +1,73 @@
 package main
 
-import (
-	"fmt"
-)
-
-//TIP <p>To run your code, right-click the code and select <b>Run</b>.</p> <p>Alternatively, click
-// the <icon src="AllIcons.Actions.Execute"/> icon in the gutter and select the <b>Run</b> menu item from here.</p>
-// RUN IT <icon src="AllIcons.Actions.Execute"/>. RUN IT <icon src="AllIcons.Actions.Execute"/>. RUN IT <icon src="AllIcons.Actions.Execute"/>. RUN IT <icon src="AllIcons.Actions.Execute"/>. RUN IT <icon src="AllIcons.Actions.Execute"/>. RUN IT <icon src="AllIcons.Actions.Execute"/>. RUN IT <icon src="AllIcons.Actions.Execute"/>. RUN IT <icon src="AllIcons.Actions.Execute"/>. RUN IT. RUN IT. RUN IT. RUN IT. RUN IT. RUN IT. RUN IT. RUN IT.
-//
+/**func printFullNumber(a int) string {
+	if a%2 == 0 {
+		return "Чётное число"
+	}
+	return "Не чётное число"
+}
 
 func main() {
-	//TIP <p>Press <shortcut actionId="ShowIntentionActions"/> when your caret is at the underlined text
-	// to see how GoLand suggests fixing the warning.</p><p>Alternatively, if available, click the lightbulb to view possible fixes.</p>
-	s := "gopher"
-	fmt.Println("Hello and welcome, %s!", s)
+	result := printFullNumber(91)
+	fmt.Println(result)
+}**/
 
-	for i := 1; i <= 100; i++ {
-		//TIP <p>To start your debugging session, right-click your code in the editor and select the Debug option.</p> <p>We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-		// for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.</p>
-		//fmt.Println("i =", 100/i)
-		love(i)
+/**func printFullNumber2(a int) string {
+	if a%2 == 0 {
+		return "Чётное число"
+	} else {
+		return "Нечётное число"
 	}
 }
 
-func some() {
+func main() {
+	fmt.Println(printFullNumber2(10))
+}**/
 
-}
+//- (switch) написать функцию, которая принимает целое число от 1 до 5 и возвращает текстовое сообщение (one, two, three, four, five) соответственно
 
-func some1() {
-
-}
-
-func some2() {
-
-}
-
-func some3() {
-
-}
-
-func some4() {
-
-}
-
-func some5() {
-
-}
-
-func love(value_of_love int) {
-	fmt.Println("I'm love you soooooo much. and I love sex with you. A LOT OF SEEEEX. И жопку твою люблю.")
-	fmt.Printf("Love loading... %d! \n", value_of_love)
-	if value_of_love == 100 {
-		fmt.Println("Love loading complete!")
-		fmt.Println("Время спонтанного секса!")
+/**func fiveDays(day int) {
+	switch day {
+	case 1:
+		fmt.Println("one")
+	case 2:
+		fmt.Println("two")
+	case 3:
+		fmt.Println("three")
+	case 4:
+		fmt.Println("four")
+	case 5:
+		fmt.Println("five")
 	}
 }
 
-aaa
+func main() {
+	fiveDays(3)
+}**/
+
+//- (for) написать функцию, которая принимает целое число 10, в цикле от 1 до 10 печает сообщение "current number is: <number>", где number -- текущее число в цикле. должно быть 10 сообщений
+
+/**func cycle10(a int) {
+	var b int
+	for b = 1; b <= a; b++ { // b = 1 - начинаем переменную с 1, b <= a - b меньше или равно а, b++ -увеличиваем i на 1 после каждой итерации
+		fmt.Println("current number is:", b)
+	}
+}
+
+func main() {
+	cycle10(10) // передаём 10 в функцию
+}**/
+
+//- (while) сделать то же самое, используя конструкцию while
+
+/**- (for + array) написать функцию, которая принимает входной массив
+myArray := []int{1, 2, 3, 4, 5}
+и с помощью цикла for выводить каждое значение в консоль/**
+
+/**- (map) написать функцию, которая принимает map
+myMap := map[string]int{
+"apple":  5,
+"banana": 3,
+"cherry": 7,
+}
+и выводит в консоль все связки ключ + значение**/
